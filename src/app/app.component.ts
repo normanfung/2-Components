6d55bb8c4fe6008a2b0b407a93721fea35259b12
normanfung <normanfung96@gmail.com>
2022-06-26 22:22:33 +0800

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '2-Components';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountain',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'nature',
+      content: 'I ride someone today',
+    },
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountain',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'nature',
+      content: 'I ride someone today',
+    },
+  ];
 }
